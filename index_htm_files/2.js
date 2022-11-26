8edce9bac8aa9cdee99f717ee27b9c4d0f309d5a
vannalux <remtexvanna@gmail.com>
2022-11-27 00:16:12 +0200

    55

---
+++ index_htm_files/2.js
@@ -0,0 +1,60 @@
+window.local_imageGallerySimple_v11_3_data = [
+{
+ border: "4",
+ cols: "3",
+ effect: "0",
+ popupsize: "750",
+ rotation: "-360",
+ shadowblur: "4",
+ shadowdim: "4",
+ trans: "0.3"
+},
+{
+ image: "index_htm_files/115.jpg"
+},
+{
+ image: "index_htm_files/116.jpg"
+},
+{
+ image: "index_htm_files/117.jpg"
+},
+{
+ image: "index_htm_files/118.jpg"
+},
+{
+ image: "index_htm_files/119.jpg"
+},
+{
+ image: "index_htm_files/120.jpg"
+}
+];
+window.wlocal_imageGallerySimple_v11_3_data = [
+{
+ border: "4",
+ cols: "3",
+ effect: "0",
+ popupsize: "750",
+ rotation: "-360",
+ shadowblur: "4",
+ shadowdim: "4",
+ trans: "0.3"
+},
+{
+ image: "index_htm_files/115.webp"
+},
+{
+ image: "index_htm_files/116.webp"
+},
+{
+ image: "index_htm_files/117.webp"
+},
+{
+ image: "index_htm_files/118.webp"
+},
+{
+ image: "index_htm_files/119.webp"
+},
+{
+ image: "index_htm_files/120.webp"
+}
+];
