3f9e22e41a27b7aeef664a299977875831ab5af2
vannalux <remtexvanna@gmail.com>
2022-11-26 23:47:36 +0200

    55

---
+++ index_htm_files/2.js
@@ -0,0 +1,30 @@
+window.local_imageGallerySimple_v11_3_data = [
+{
+ border: "4",
+ cols: "3",
+ effect: "0",
+ popupsize: "750",
+ rotation: "-360",
+ shadowblur: "4",
+ shadowdim: "4",
+ trans: "0.3"
+},
+{
+ image: "index_htm_files/106.jpg"
+},
+{
+ image: "index_htm_files/107.jpg"
+},
+{
+ image: "index_htm_files/108.jpg"
+},
+{
+ image: "index_htm_files/109.jpg"
+},
+{
+ image: "index_htm_files/110.jpg"
+},
+{
+ image: "index_htm_files/111.jpg"
+}
+];
